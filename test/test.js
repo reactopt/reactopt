147539b5592820bffc6b7cfaf668475627f7a1c2
szmoon <szmoon@gmail.com>
2017-11-01 10:50:05 -0700

    remove chrome launcher and dependencies

--- test/test.js
+++ test/test.js
@@ -1,13 +1,2 @@
-const chromeLauncher = require('chrome-launcher');
-
-
-let uri = process.argv[2];
-
-// let uri = 'https://google.com';
-let url; 
-chromeLauncher.launch({
-  startingUrl: uri
-}).then(chrome => {
-  url = chrome.port;
-  console.log(`Chrome debugging port running on ${chrome.port}`);
-});
+console.log("test.js running!");
+  
