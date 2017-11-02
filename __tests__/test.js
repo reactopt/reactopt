78125bc420818366d7846231c998f7b32b95a522
szmoon <szmoon@gmail.com>
2017-11-02 11:20:05 -0700

    delete other test folder

--- __tests__/test.js
+++ __tests__/test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
     const wrapper = shallow(<App/>);
     console.log(wrapper);
 })
+
+// // let uri = process.argv[2]; // gets url from CLI "npm start [url]"
+// let uri = './../../css-color-palatte' //test uri for local app folder
+// console.log(uri);
