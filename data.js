9459e092d9e139e0a9e11b00a5169282e2d02775
szmoon <szmoon@gmail.com>
2017-11-07 20:13:41 -0800

    removed puppeteer code

--- data.js
+++ data.js
@@ -1,11 +1,14 @@
 let data = {
-
 hardcodedTestEventName:{
   Middle:"Middle",
   SavePalette:"SavePalette",
   Top:"Top"
+  },
+  secondHardcodedEvent:{
+    Top:"Top",
+    Bottom:"Bottom",
+    LoadPalette:"LoadPalette"
   }
-  
 };
 
 export {data};
