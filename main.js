71e217a7a04753fd2e1bef479610320875836dfc
Vu Phung <vphung5707@gmail.com>
2017-11-29 15:03:50 -0800

    update test case in test/index.js, add exports function to test in main.js, and add devDep for test framework in package.json.

--- main.js
+++ main.js
@@ -138,4 +138,5 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-exports.data = data;
+// exports.data = data;
+module.exports = { data, loadTime, printLine, componentRerenders };
